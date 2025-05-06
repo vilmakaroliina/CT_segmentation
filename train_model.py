1266cc733b37f46f4d3ba0e9f714ee8434cfc4b5
Vilma Lehto <vilmalehto@Vilmas-MacBook-Air.local>
2025-05-06 15:11:12 +0300

    Basic UNet and training code

---
+++ train_model.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  6 12:59:12 2025
+
+@author: vilmalehto
+
+Model training file. The code trains the model and saves it at the end to
+the path defined by user. 
+
+To train the model you need to know the path for training data. Currently the 
+code divides the data to 80% training and 20% validation data.
+
+KEEP THE TESTING DATA IN DIFFERENT FOLDER AND DON'T GIVE THAT PATH TO MODEL HERE!
+"""
+
+import torch
+from torch import optim, nn
+from torch.utils.data import DataLoader, random_split
+from tqdm import tqdm
+
+from unet import UNet #our unet class
+#from datapreparation import Dataset #this doesn't exist yet, but the data have to be prepared
+
+if __name__ == "__main__":
+    
+    #set the parameters
+    LEARNING_RATE = 0.0001 
+    BATCH_SIZE = 1
+    EPOCHS = 10
+    DATA_PATH = "/data" #set the correct path as I get the  data
+    MODEL_SAVE = "/models/unet.pth" #set also this once you have the data 
+        #and you can run this, I don't know if it have to be an empty folder
+    
+    #use GPU if available
+    device = torch.device("mps" if torch.mps.is_available() else "cpu") #this is mac specific GPU change it to "cuda" for NVIDIA GPU:s
+    
+    #prepare the datasets
+    train_dataset = Dataset(DATA_PATH) #not working yet as the data preparation isn't done
+    
+    #divide the set to train and validation
+    generator = torch.Generator().manual_seed(42) #to ensure the reproductibility, I could also just store the data to different places
+    train_set, val_set = random_split(train_dataset, [0.8, 0.2], generator = generator)
+    
+    #create the dataloaders
+    train_dataloader = DataLoader(dateset = train_set,
+                                  batch_size = BATCH_SIZE,
+                                  shuffle = True)
+    
+    val_dataloader = DataLoader(dataset = val_set,
+                                batch_size  = BATCH_SIZE,
+                                shuffle = True)
+    
+    #define the model
+    #in_channels = 1 for grayscale and 3 for RGB
+    #num_classes = 4, skull, CSF, brain and haemorrhage
+    model = UNet(in_channels = 1, num_classes = 1).to(device)
+    optimizer = optim.Adam(model.parameters(), lr = LEARNING_RATE) #start with Adam optimizer as we have used that in courses
+    criterion = nn.BCEWithLogitsLoss() #sets up the loss function, also this 
+        #is binary loss function -> have to modified based on the literature
+        
+    for epoch in tqdm(range(EPOCHS)):
+        model.train()
+        train_running_loss = 0
+        
+        for idx, img_mask in enumerate(tqdm(train_dataloader)):
+            img = img_mask[0].float().to(device)
+            mask = img_mask[1].float().to(device)
+            
+            #predict the segments
+            y_pred = model(img)
+            optimizer.zero_grad() #no idea what this does
+            
+            #count the loss
+            loss = criterion(y_pred, mask) #comapre the manual segmentation and prediction
+            train_running_loss += loss.item()
+            
+            #these are also mystery
+            loss.backward()
+            optimizer.step()
+            #but here end the train epoch
+            
+        #on to validation epoch
+        train_loss = train_running_loss / idx+1
+        
+        model.eval()
+        val_runnin_loss = 0
+        with torch.no_grad():
+            for idx, img_mask in enumerate(tqdm(val_dataloader)):
+                img = img_mask[0].float().to(device)
+                mask = img_mask[1].float().to(device)
+                
+                y_pred = model(img)
+                loss = criterion(y_pred, mask)
+                
+                val_runnin_loss += loss.item()
+                
+            val_loss = val_runnin_loss / idx+1
+            
+        #print for every epoch
+        print("-"*30) #kuha näitä on enemmmä ku merkkejä tekstissä
+        print(f"EPOCH {epoch +1 }")
+        print(f"Train loss {train_loss:.4f}")
+        print(f"Validation loss {val_loss:.4f}")
+        
+    torch.save(model.state_dict(), MODEL_SAVE)
+    
+    
+    
+    
+        
+            
+    
---
+++ unet.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  6 08:59:13 2025
+
+@author: vilmalehto
+"""
+
+import torch
+import torch.nn as nn
+
+#tupla konvoluutio, eli ne kaksi vaakatason nuolta mallin kuvassa
+class ConvolutionBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__() #super init to construct the parent class
+        
+        self.conv_output = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size = 3, padding = 1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace = True),
+            nn.Conv2D(out_channels, out_channels, kernel_size = 3, padding = 1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace =  True))
+        
+        def forward(self, x):
+            return self.conv_ouput(x)
+        
+    
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        
+        self.convolution = ConvolutionBlock(in_channels, out_channels)
+        self.downsample = nn.MaxPool2d(kernel_size = 2, stride = 2) 
+        
+        #Encoder block has two outputs one for the U-path and one for skip connection
+        #The skip connection output is the convolution output
+        #and the U-path output goes through the MaxPooling
+        def forward(self, x):
+            conv_output = self.convolution(x)
+            pooling = self.downsample(conv_output)
+            
+            return conv_output, pooling
+        
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init()
+        
+        self.up = nn.ConvTransposed2d(in_channels, in_channels//2, kernel_size = 2, stride = 2)
+        self.conv = ConvolutionBlock(in_channels, out_channels)
+        
+        #the inputs for decoder block are the output from previous 
+        #encoder/decoder block and the input from skipconnection
+        def forward(self, x1, x2): 
+            up = self.up(x1) #upsample the input form u-path
+            comb = torch.cat([up, x2], 1) #concatenate the inputs
+            op = self.conv(comb) #do a convolution for the concatenated part
+            return op
+            
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        
+        #The downsample path
+        self.down_conv1 = EncoderBlock(in_channels, 64)
+        self.down_conv2 = EncoderBlock(64, 128)
+        self.down_conv3 = EncoderBlock(128, 256)
+        self.down_conv4 = EncoderBlock(256, 512)
+        
+        #The bottle neck
+        self.bottle_neck = ConvolutionBlock(512, 1024)
+        
+        #The upsampling path
+        self.up_conv1 = DecoderBlock(1024, 512)
+        self.up_conv2 = DecoderBlock(512, 256)
+        self.up_conv3 = DecoderBlock(256, 128)
+        self.up_conv4 = DecoderBlock(128, 64)
+        
+        #The output layer
+        self.output = nn.Conv2d(in_channels = 64, out_channels = num_classes, kernel_size = 1)
+        
+        def forward(self, x):
+            
+            skip1, p1 = self.down_conv1(x)
+            skip2, p2 = self.down_conv2(p1)
+            skip3, p3 = self.down_conv3(p2)
+            skip4, p4 = self.down_conv4(p3)
+            
+            b = self.bottle_neck(p4)
+            
+            up1 = self.up_conv1(b, skip4)
+            up2 = self.up_conv2(up1, skip3)
+            up3 = self.up_conv3(up2, skip2)
+            up4 = self.up_conv4(up3, skip1)
+            
+            output = self.output(up4)
+            return output
+            
+        
+        
+    
